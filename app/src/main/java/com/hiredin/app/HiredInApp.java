7baea9c10b4c7e524c0e1e3f07165b51c9cd651a
MohammadMasir <send2masir@gmail.com>
2025-02-15 22:04:02 +0530

    1st commit.

---
+++ app/src/main/java/com/hiredin/app/HiredInApp.java
@@ -0,0 +1,13 @@
+package com.hiredin.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiredInApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiredInApp.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/hiredin/app/HiredInAppTests.java
@@ -0,0 +1,13 @@
+package com.hiredin.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiredInAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
